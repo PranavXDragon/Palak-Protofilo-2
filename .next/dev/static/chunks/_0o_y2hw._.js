12dd1fd856c755019e8726819a975489181525da
Pranav Navghare <pranavnavghare46@gmail.com>
2026-04-20 00:58:38 +0530

    Fix profile image display in About section

--- .next/dev/static/chunks/_0o_y2hw._.js
+++ .next/dev/static/chunks/_0o_y2hw._.js
@@ -193,7 +193,7 @@ const About = ()=>{
                 className: "about__container container grid",
                 children: [
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                        src: __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$assets$2f$profile$2d$pic$2e$png$2e$mjs__$7b$__IMAGE__$3d3e$__$225b$project$5d2f$src$2f$assets$2f$profile$2d$pic$2e$png__$28$static__in__ecmascript$2c$__tag__client$2922$__$7d$__$5b$app$2d$client$5d$__$28$structured__image__object__with__data__url$2c$__ecmascript$29$__["default"],
+                        src: __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$assets$2f$profile$2d$pic$2e$png$2e$mjs__$7b$__IMAGE__$3d3e$__$225b$project$5d2f$src$2f$assets$2f$profile$2d$pic$2e$png__$28$static__in__ecmascript$2c$__tag__client$2922$__$7d$__$5b$app$2d$client$5d$__$28$structured__image__object__with__data__url$2c$__ecmascript$29$__["default"].src,
                         alt: "Profile",
                         className: "about__img"
                     }, void 0, false, {
--- components/about/About.jsx
+++ components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
         <span className="section__subtitle">My Intoduction</span>
 
         <div className="about__container container grid">
-          <img src={ProfilePic} alt="Profile" className="about__img" />
+          <img src={ProfilePic.src} alt="Profile" className="about__img" />
           <div className="about__data">
             <Info />
 
