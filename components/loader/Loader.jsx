38c79a1411b93eba5ee76f1ea6377b9a7aae94ed
Pranav Navghare <pranavnavghare46@gmail.com>
2026-04-20 01:37:15 +0530

    Add loading spinner component with three-dot bounce animation

---
+++ components/loader/Loader.jsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import './loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__dot"></div>
+      <div className="loader__dot"></div>
+      <div className="loader__dot"></div>
+    </div>
+  );
+};
+
+export default Loader;
