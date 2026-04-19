b45180c8cb470978d4c219b6ce8310c9417a4c06
Pranav Navghare <pranavnavghare46@gmail.com>
2026-04-20 01:40:09 +0530

    Update Social component to match original portfolio structure

--- components/home/Social.jsx
+++ components/home/Social.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/isha-urkude" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/isha-urkude" className="home__social-icon" target="_blank">
             <i className="uil uil-linkedin"></i>
         </a>
 
-        <a href="https://github.com" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com" className="home__social-icon" target="_blank">
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
